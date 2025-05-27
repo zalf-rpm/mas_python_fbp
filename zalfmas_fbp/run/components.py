329966605a9c0563b8dc7c573754c94a1095d3ae
Michael Berg-Mohnicke <michael.berg@zalf.de>
2025-05-27 14:07:16 +0200

    moved Component class to local_components_service module

--- zalfmas_fbp/run/components.py
+++ zalfmas_fbp/run/components.py
@@ -100,51 +100,8 @@ def handle_default_fpb_component_args(parser, config: dict=None):
     return port_infos_reader_sr, config, args
 
 
-def start_component(path_to_executable, port_infos_reader_sr):
+def start_local_component(path_to_executable, port_infos_reader_sr):
     proc = sp.Popen(list(path_to_executable.split(" ")) + [port_infos_reader_sr],
                     #stdout=sp.PIPE, stderr=sp.STDOUT,
                     text=True)
     return proc
-
-class Component(fbp_capnp.Component.Server, common.Identifiable):
-
-    def __init__(self, path_to_executable, id=None, name=None, description=None, admin=None, restorer=None):
-        common.Identifiable.__init__(self, id=id, name=name, description=description)
-        self.path_to_executable = path_to_executable
-        self.proc = None
-
-    async def start(self, portInfosReaderSr, _context):  # start @0 (portInfosReaderSr :Text) -> (success :Bool);
-        self.proc = start_component(self.path_to_executable, portInfosReaderSr)
-        return self.proc.poll() is None
-
-    async def stop(self, _context):  # stop @0 () -> (success :Bool);
-        if self.proc and self.proc.poll() is None:
-            self.proc.terminate()
-            rt = self.proc.returncode == 0
-            self.proc = None
-            return rt
-        return False
-
-async def main(path_to_executable=None, serve_bootstrap=True, host=None, port=None,
-               id=None, name="FBP component service", description=None, srt=None):
-    config = {
-        "path_to_executable": path_to_executable,
-        "port": port,
-        "host": host,
-        "id": id,
-        "name": name,
-        "description": description,
-        "serve_bootstrap": serve_bootstrap,
-        "srt": srt
-    }
-    common.update_config(config, sys.argv, print_config=True, allow_new_keys=False)
-
-    restorer = common.Restorer()
-    service = Component(config["path_to_executable"], id, name, description)
-    name_to_service = {"service": service}
-    await serv.init_and_run_service(name_to_service, config["host"], config["port"],
-                                    serve_bootstrap=config["serve_bootstrap"], restorer=restorer,
-                                    name_to_service_srs={"service": config["srt"]})
-
-if __name__ == '__main__':
-    asyncio.run(capnp.run(main(serve_bootstrap=True)))
