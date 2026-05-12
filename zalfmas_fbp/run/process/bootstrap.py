5b3ef6b4a3ad8c4752be544ffa46b75146d69589
Vincent Dlugosch <dlugoschvincent@gmail.com>
2026-05-12 15:17:35 +0200

    chore: make bootstrap fail faster as metadata is now schema validated anyways

--- zalfmas_fbp/run/process/bootstrap.py
+++ zalfmas_fbp/run/process/bootstrap.py
@@ -1,15 +1,11 @@
 from __future__ import annotations
 
-import logging
-
 from zalfmas_fbp.run.metadata import ComponentMetadata, ComponentPortMetadata
 
 from .config.config_runtime import ProcessConfigRuntime
 from .context import ProcessPortState
 from .identity import WritableProcessIdentityContext
 from .types import ConfigValue, ProcessConfig, RawConfig
-
-logger = logging.getLogger(__name__)
 
 
 class ProcessBootstrap[ConfigT: ProcessConfig | RawConfig]:
@@ -34,31 +30,25 @@ class ProcessBootstrap[ConfigT: ProcessConfig | RawConfig]:
         default_config: dict[str, ConfigValue] = {}
         component_meta = self._metadata
         if component_meta is not None:
-            try:
-                default_config = component_meta.default_config_values()
-                self._identity.name = component_meta.info.name
-                if component_meta.info.description is not None:
-                    self._identity.description = component_meta.info.description
-                for port_info in component_meta.inPorts:
-                    name = port_info.name
-                    if self._is_array_port(port_info):
-                        _ = self._ports.array_in_ports.setdefault(name, [])
-                        _ = self._ports.array_in_buffers.setdefault(name, {})
-                    else:
-                        _ = self._ports.in_ports.setdefault(name, None)
-                for port_info in component_meta.outPorts:
-                    name = port_info.name
-                    if self._is_array_port(port_info):
-                        _ = self._ports.array_out_ports.setdefault(name, [])
-                        _ = self._ports.array_out_next_indices.setdefault(name, 0)
-                        _ = self._ports.array_out_write_tasks.setdefault(name, [])
-                    else:
-                        _ = self._ports.out_ports.setdefault(name, None)
-            except (KeyError, TypeError, ValueError) as error:
-                logger.warning(
-                    "Some metadata could not be used for initializing the process component. Exception: %s",
-                    error,
-                )
+            default_config = component_meta.default_config_values()
+            self._identity.name = component_meta.info.name
+            if component_meta.info.description is not None:
+                self._identity.description = component_meta.info.description
+            for port_info in component_meta.inPorts:
+                name = port_info.name
+                if self._is_array_port(port_info):
+                    _ = self._ports.array_in_ports.setdefault(name, [])
+                    _ = self._ports.array_in_buffers.setdefault(name, {})
+                else:
+                    _ = self._ports.in_ports.setdefault(name, None)
+            for port_info in component_meta.outPorts:
+                name = port_info.name
+                if self._is_array_port(port_info):
+                    _ = self._ports.array_out_ports.setdefault(name, [])
+                    _ = self._ports.array_out_next_indices.setdefault(name, 0)
+                    _ = self._ports.array_out_write_tasks.setdefault(name, [])
+                else:
+                    _ = self._ports.out_ports.setdefault(name, None)
 
         filtered_defaults = {key: value for key, value in default_config.items() if value is not None}
         self._config_runtime.apply_config_values(filtered_defaults)
