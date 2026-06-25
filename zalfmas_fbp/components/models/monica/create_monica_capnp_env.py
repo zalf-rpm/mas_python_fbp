b77e92e116c563ce0b74411f92f5f20412bf2b27
Michael Berg-Mohnicke <michael.berg@zalf.de>
2026-06-25 16:21:25 +0200

    Update create_monica_capnp_env.py

--- zalfmas_fbp/components/models/monica/create_monica_capnp_env.py
+++ zalfmas_fbp/components/models/monica/create_monica_capnp_env.py
@@ -109,9 +109,9 @@ METADATA = meta.Component(
 
 class Component(process.Process[Config]):
     def __init__(
-            self,
-            metadata: meta.Component = METADATA,
-            con_man: common.ConnectionManager | None = None,
+        self,
+        metadata: meta.Component = METADATA,
+        con_man: common.ConnectionManager | None = None,
     ):
         super().__init__(metadata=metadata, con_man=con_man)
 
